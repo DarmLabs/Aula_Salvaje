6a1c24dd2adfd500d3731ae148eb6950a402c304
BDeWinne <68663886+BDeWinne@users.noreply.github.com>
2021-07-21 12:11:37 -0300

    Add files

---
+++ Assets/Scripts/Map/MapManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver() 
+    {
+         Debug.Log("Mouse is over GameObject."+ gameObject.name);
+    }
+
+
+}
