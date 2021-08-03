2172f32f675a627064f460f7e978929e17525029
BDeWinne <68663886+BDeWinne@users.noreply.github.com>
2021-08-03 13:50:52 -0300

    fix: Zoom In & Out
    Error conocido: Apreta click izq y escape a la vez bugea las animaciones

--- Assets/Scripts/Map/MapManager.cs
+++ Assets/Scripts/Map/MapManager.cs
@@ -1,97 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class MapManager : MonoBehaviour
 {
-    Material Default;
-    public Material LightMaterial;
-    GameObject Name;
-    Vector3 mousePosition;
-    public GameObject MainCamera;
-    string currentProvincia;
-    bool Using = false;
+    Ray ray;
+    RaycastHit hit;
+    public string currentProvincia;
+    public bool Zoom = false;
+
     void Start()
     {
-        Name = GameObject.Find("Name");
-        MainCamera = GameObject.Find("Main Camera");
-        Default = gameObject.GetComponent<MeshRenderer>().material;
-        LightMaterial = Resources.Load("Materials/LightMaterial", typeof(Material)) as Material;
+        
     }
-
     void Update()
     {
-        mousePosition = Input.mousePosition;
-        Name.transform.position = mousePosition;
-        if(Input.GetMouseButton(0))
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit))
         {
-            OffProvincias();
-        }
-        Controls();
-    }
-
-    void OnMouseEnter()
-    {
-        if(!Using)
-        {
-            Name.GetComponent<Text>().text = (gameObject.name);
-            gameObject.GetComponent<MeshRenderer>().material = LightMaterial;
-        }
-    }
-    void OnMouseExit()
-    {
-        gameObject.GetComponent<MeshRenderer>().material = Default;
-        Name.GetComponent<Text>().text = "";
-    }
-    void OnMouseOver()
-    {
-        if(Input.GetMouseButton(0))
-        {
-            MainCamera.GetComponent<Animator>().Play(gameObject.name);
-            Using = true;
-            MainCamera.GetComponent<UI_Manager>().TurnOffText();
-            gameObject.GetComponent<MeshRenderer>().material = Default;
-            
-        }
-    }
-    void Controls()
-    {
-        if(Input.GetKeyDown(KeyCode.Escape))
-        {
-            MainCamera.GetComponent<Animator>().Play(gameObject.name + " 0");
-            Using = false;
-            OnProvincias();
-        }
-    }   
-    void OnProvincias()
-    {
-        if(!Using)
-        {
-            gameObject.GetComponent<MeshRenderer>().enabled = true;
-            if(gameObject.GetComponent<MeshCollider>() != null)
+            if(Input.GetMouseButtonDown(0) && !Zoom)
             {
-                gameObject.GetComponent<MeshCollider>().enabled = true;
+                if(hit.transform.gameObject.tag == "Provincia")
+                {
+                    currentProvincia = hit.transform.gameObject.name;
+                    GetComponent<Animator>().Play(currentProvincia);
+                    Zoom = true;
+                    GetComponent<UI_Manager>().TurnOffText();
+                }
             }
-            if(gameObject.GetComponent<BoxCollider>() != null)
+        }
+        if(Zoom)
+        {
+            if(Input.GetKeyDown(KeyCode.Escape))
             {
-                gameObject.GetComponent<BoxCollider>().enabled = true;
+                GetComponent<Animator>().Play(currentProvincia + " 0");
+                StartCoroutine(zoomOutEnds(1));
             }
         }
     }
-    void OffProvincias()
+    IEnumerator zoomOutEnds(int secs)
     {
-        if(!Using)
-        {
-            gameObject.GetComponent<MeshRenderer>().enabled = false;
-            if(gameObject.GetComponent<MeshCollider>() != null)
-            {
-                gameObject.GetComponent<MeshCollider>().enabled = false;
-            }
-            if(gameObject.GetComponent<BoxCollider>() != null)
-            {
-                gameObject.GetComponent<BoxCollider>().enabled = false;
-            }
-        }
+        yield return new WaitForSeconds(secs);
+        Zoom = false;
+        GetComponent<UI_Manager>().TurnOnText();
     }
 }
