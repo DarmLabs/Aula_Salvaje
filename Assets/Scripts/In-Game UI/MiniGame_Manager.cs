5538487b91b97a34cf2f7c008370b62cb2306cc4
BDeWinne <68663886+BDeWinne@users.noreply.github.com>
2021-08-12 10:42:30 -0300

    feat: tuto en cada minijuego

--- Assets/Scripts/In-Game UI/MiniGame_Manager.cs
+++ Assets/Scripts/In-Game UI/MiniGame_Manager.cs
@@ -6,18 +6,32 @@ using UnityEngine.SceneManagement;
 public class MiniGame_Manager : MonoBehaviour
 {
     public GameObject Panel;
+    void Start()
+    {
+        Time.timeScale = 0;
+        StartCoroutine(disableTuto(3));
+    }
     public void PauseMenuOn()
     {
         Panel.SetActive(true);
         Panel.transform.GetChild(0).gameObject.SetActive(true);
+        Time.timeScale = 0;
     }
     public void PauseMenuOff()
     {
         Panel.SetActive(false);
         Panel.transform.GetChild(0).gameObject.SetActive(false);
+        Time.timeScale = 1;
     }
     public void Exit()
     {
         SceneManager.LoadScene(1);
+    }
+    IEnumerator disableTuto(int secs)
+    {
+        yield return new WaitForSecondsRealtime(secs);
+        Panel.transform.GetChild(1).gameObject.SetActive(false);
+        Panel.SetActive(false);
+        Time.timeScale = 1;
     }
 }
