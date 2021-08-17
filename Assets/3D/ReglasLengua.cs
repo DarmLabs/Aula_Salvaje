5217d63f0e962bdb1726c7abd7749d995fe8823b
julagui <julioaguilar-ap@outlook.com>
2021-08-17 18:09:20 -0300

    PuedeDibujar1Linea

--- Assets/3D/ReglasLengua.cs
+++ Assets/3D/ReglasLengua.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class ReglasLengua : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private int contadorHormigas;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
         
@@ -19,5 +19,11 @@ public class ReglasLengua : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other) 
     {
        Debug.Log("Colisiono con:   "+other.gameObject.name+"  "+other.gameObject.tag);
+       if (other.gameObject.tag =="Alimentacion")
+       {
+           Destroy(other.gameObject);
+           contadorHormigas+=1;
+           Debug.Log("Hormigas comidas:   "+contadorHormigas);
+       }
     }
 }
