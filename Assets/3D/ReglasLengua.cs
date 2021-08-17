9a7979f12b787acfdf43d2a8a34b278bb983e687
julagui <julioaguilar-ap@outlook.com>
2021-08-17 18:24:40 -0300

    Correccion

--- Assets/3D/ReglasLengua.cs
+++ Assets/3D/ReglasLengua.cs
@@ -5,19 +5,29 @@ using UnityEngine;
 public class ReglasLengua : MonoBehaviour
 {
     // Start is called before the first frame update
+    private int contadorHormigas;
     void Start()
     {
         
     }
 
     // Update is called once per frame
+   
     void Update()
     {
-        
     }
+        
 
     void OnCollisionEnter2D(Collision2D other) 
     {
        Debug.Log("Colisiono con:   "+other.gameObject.name+"  "+other.gameObject.tag);
+       if (other.gameObject.tag =="Alimentacion")
+       {
+           Destroy(other.gameObject);
+           contadorHormigas+=1;
+           Debug.Log("Hormigas comidas:   "+contadorHormigas);
+       }
     }
+
+    
 }
