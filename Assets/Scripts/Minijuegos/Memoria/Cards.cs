ab5ac92825f93cde85082ba960e69f0acedd6cad
julagui <julioaguilar-ap@outlook.com>
2021-08-17 16:24:55 -0300

    Memory Fix

--- Assets/Scripts/Minijuegos/Memoria/Cards.cs
+++ Assets/Scripts/Minijuegos/Memoria/Cards.cs
@@ -38,7 +38,7 @@ public class Cards : MonoBehaviour
         if(!facedUp)
         {
             sequence.Enqueue(this);
-            for (float i = 0f; i < 180f; i+=10)
+            for (float i = 0f; i < 190f; i+=10)
             {
                 transform.rotation = Quaternion.Euler(0f,i,0f);
                 yield return new WaitForSeconds(0f);
@@ -46,7 +46,7 @@ public class Cards : MonoBehaviour
         }
         else if (facedUp)
         {
-            for (float i = 180f; i >= 0f; i-=10)
+            for (float i = 190f; i >= 0f; i-=10)
             {
                 transform.rotation=Quaternion.Euler(0f,i,0f);
                 yield return new WaitForSeconds(0f);
@@ -94,7 +94,7 @@ public class Cards : MonoBehaviour
     {
         coroutineAllowed =false;
         yield return new WaitForSeconds(0.2f);
-        for (float i=180f; i>=0f; i-=10)
+        for (float i=190f; i>=0f; i-=10)
         {
             transform.rotation =Quaternion.Euler(0f,i,0f);
             yield return new WaitForSeconds(0f);
